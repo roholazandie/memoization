554e5dead560de3cb56aede2b749eed216b00eb2
rohola <hilbert.cantor@gmail.com>
2019-09-03 10:04:29 -0600

    added implement decorator

---
+++ implement_decorator.py
@@ -0,0 +1,24 @@
+import functools
+
+def memoize(func):
+    cache = func.cache = {}
+    @functools.wraps(func)
+    def memoized_func(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return memoized_func
+
+@memoize
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+if __name__ == "__main__":
+    print(fibonacci(100))
